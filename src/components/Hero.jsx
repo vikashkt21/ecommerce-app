2d31ec1d0da8eb2e8a69a0582e4e529ed2f6e5c7
vikashkt21 <vikashkatiyar966@gmail.com>
2025-03-18 19:34:36 +0530

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,9 +9,9 @@ const Hero = () => {
         <div className="text-[#414141]">
           <div className="flex items-center gap-2">
             <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
-            <p className="font-medium text-sm md:text-base">our best seller</p>
+            <p className="font-medium text-sm md:text-base">OUR BESTSELLER</p>
           </div>
-          <h1 className="text-3xl sm:py-3 lg:text-5xl leading-relaxed">
+          <h1 className="prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed">
             Latest Arrivals
           </h1>
           <div className="flex items-center gap-2">
---
+++ src/components/LatestCollection.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
+
+const LatestCollection = () => {
+
+  const {products}=useContext(ShopContext);
+  
+  return (
+    <div className='my-10'>
+        <div className='text-center py-8 text-3xl'>
+            <Title text1={'LATEST'} text2={'COLLECTION'} />
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default LatestCollection
